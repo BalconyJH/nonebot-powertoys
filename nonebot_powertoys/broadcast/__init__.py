b264be2fc702cd7945dc1a0e4ea10398381e46f0
BalconyJH <balconyjh@gmail.com>
2023-11-24 19:45:04 +0800

    :zap: 调整broadcast函数结构

--- nonebot_powertoys/broadcast/__init__.py
+++ nonebot_powertoys/broadcast/__init__.py
@@ -39,34 +39,43 @@ test = on_command("test", priority=1, permission=SUPERUSER, block=True)
 
 @broadcast.handle()
 async def _(
-    bot: Union[V11Bot, V12Bot],
-    event: Union[V11MessageEvent, V12MessageEvent],
-    arg: Union[V11Message, V12Message] = CommandArg(),
-    img_list: List = image_list(),
+        bot: Union[V11Bot, V12Bot],
+        event: Union[V11MessageEvent, V12MessageEvent],
+        arg: Union[V11Message, V12Message] = CommandArg(),
+        img_list: List = image_list(),
 ):
     msg = arg.extract_plain_text().strip()
-    rst = []
-    for img in img_list:
-        rst += saa.Image(img)
+    rst = [saa.Image(img) for img in img_list]
     gl = [g["group_id"] for g in await bot.get_group_list()]
-    g_cnt = len(gl)
-    cnt = 0
-    error = ""
-    x = 0.25
-    for g in gl:
-        cnt += 1
-        if cnt / g_cnt > x:
-            await broadcast.send(f"已播报至 {int(cnt / g_cnt * 100)}% 的群聊")
-            x += 0.25
+
+    async def send_broadcast(group_id, progress):
         try:
-            target = saa.TargetQQGroup(group_id=g)
+            target = saa.TargetQQGroup(group_id=group_id)
             msg_builder = saa.MessageFactory([saa.Text(msg), *rst])
             await msg_builder.send_to(target)
-            logger.info("投递广播成功", "广播", group_id=g)
-        except FileNotFoundError as e:
-            logger.error("投递广播失败", "广播", group_id=g, e=e)
-            error += f"GROUP {g} 投递广播失败：{e}\n"
+            logger.info("投递广播成功", "广播", group_id=group_id)
+        except Exception as e:
+            logger.error("投递广播失败", "广播", group_id=group_id, e=e)
+            return f"GROUP {group_id} 投递广播失败：{e}\n"
         await asyncio.sleep(0.5)
-    await broadcast.send("已播报至 100% 的群聊")
+        if progress:
+            await broadcast.send(f"已播报至 {progress}% 的群聊")
+
+    error = ""
+    for cnt, g in enumerate(gl, start=1):
+        _progress = int(cnt / len(gl) * 100)
+        if _progress % 25 == 0 or _progress == 100:
+            error += await send_broadcast(g, _progress)
+        else:
+            error += await send_broadcast(g, None)
+
     if error:
         await broadcast.send(f"播报时错误：{error}")
+
+
+@test.handle()
+async def handle_func(bot: Union[V11Bot, V12Bot], event: Union[V11MessageEvent, V12MessageEvent],
+                      arg: Union[V11Message, V12Message] = CommandArg()):
+    target_group = saa.TargetQQGroup(group_id=44781405)
+    msg_builder = saa.MessageFactory([saa.Text("\n管理员回复\n"), saa.Mention(user_id=str(1066474894))])
+    await msg_builder.send_to(target_group)
