733c2a48a4d5132b16084e6e52ca19f6e22ca7da
BalconyJH <balconyjh@gmail.com>
2024-01-03 23:11:30 +0800

    :seedling: 添加插件元數據

---
+++ nonebot_powertoys/__init__.py
@@ -0,0 +1,42 @@
+from nonebot import get_driver
+from nonebot.plugin import PluginMetadata, require, inherit_supported_adapters
+
+require("nonebot_plugin_saa")
+require("nonebot_plugin_user")
+require("nonebot_plugin_session")
+require("nonebot_plugin_orm")
+
+import nonebot_plugin_saa  # noqa F401
+
+nonebot_plugin_saa.enable_auto_select_bot()
+
+__help__version__ = "0.1.0"
+__help__plugin__name__ = "nonebot_powertoys"
+__help__plugin__usage__ = """
+    nonebot_powertoys.broadcast
+    nonebot_powertoys.walkie_talkie
+""".strip()
+__plugin_meta__ = PluginMetadata(
+    homepage="https://github.com/BalconyJH/nonebot-powertoys",
+    name=__help__plugin__name__,
+    description="nonebot_powertoys",
+    usage=__help__plugin__usage__,
+    supported_adapters=inherit_supported_adapters(
+        "nonebot_plugin_saa", "nonebot_plugin_user"
+    ),
+)
+
+driver = get_driver()
+
+
+@driver.on_startup
+async def _():
+    from nonebot_powertoys.plugins.broadcast import broadcast
+    from nonebot_powertoys.plugins.walkie_talkie import dialogue, reply
+
+    broadcast._plugin_meta = __plugin_meta__
+    dialogue._plugin_meta = __plugin_meta__
+    reply._plugin_meta = __plugin_meta__
+
+    from .db.db_model import create_sqlite_database
+    await create_sqlite_database()
